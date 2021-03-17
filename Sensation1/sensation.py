[Fix] overflow readout list

Sensation1/sensation.py
@@ -66,7 +66,7 @@ class Sensation(SensationBase):
         cv2.imshow(self.LogTitle,img)
         img = torch.from_numpy(img).unsqueeze(0).to(self.device).permute(0,3,2,1)
         img = self.resizer(img).type(self.torchdtype)/255
-        print('\rcurrent_length',self.current_length,'mins',self.mins,end='')
+        #print('\rcurrent_length',self.current_length,'mins',self.mins,end='')
         return img
 
     def UpdateEnd(self) -> None:

SensationBase.py
@@ -178,7 +178,7 @@ class SensationBase(MemoryManager):
         # load ReadOutMemory ,Id and Time
         self.ReadOutTime = np.zeros((self.ReadOutLength,),dtype='float64')
         if isfile(self.ReadOutId_file):
-            ml = self.load_python_obj(self.ReadOutId_file)[:self.ReadOutLength]
+            ml = self.load_python_obj(self.ReadOutId_file)[:self.KeepLength]
             if isfile(self.ReadOutMemory_file) and isfile(self.ReadOutTime_file):
                 # load id
                 current_length = ml.shape[0]