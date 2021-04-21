7016fb203a13e25498db4f92f4e683a3ff0c9939
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2021-04-21 22:10:44 +0900

    Create TrainDebug.py

---
+++ TrainDebug.py
@@ -0,0 +1,13 @@
+# ------ your setttings ------
+TrainModule = 'Sensation7' # your sensation folder name
+device = 'cuda' # debugging device
+# ------ end of settings -----
+
+if __name__ == '__main__':
+    from importlib import import_module
+    from multiprocessing import Value
+    module = import_module(TrainModule)
+    func = module.Train(device,True)
+    shutdown = Value('i',False)
+    sleep = Value('i',True)
+    func(shutdown,sleep)
